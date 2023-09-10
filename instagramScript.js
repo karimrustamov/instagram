184b9734c51fa41911691cbf8ed687b2ea0d8bad
Karim <karimrustamov@homail.de>
2023-09-10 22:17:01 +0200

    yeesh

--- instagramScript.js
+++ instagramScript.js
@@ -3,19 +3,25 @@ let posts = [
         'author': 'Max Mustermann',
         'image': './instagramImg/IGimg1.jpg',
         'location': 'Berlin, Germany',
-        'description': 'UwU'
+        'description': 'Quality time',
+        'comments': ['Cool!'],
+        'users': ['Momo']
     },
     {
         'author': 'Anna Musterfrau',
         'image': './instagramImg/IGimg2.jpg',
         'location': 'München, Germany',
-        'description': 'Der Wahnsinn!'
+        'description': 'Der Wahnsinn!',
+        'comments': [],
+        'users': []
     },
     {
         'author': 'Murat Musterfa',
         'image': './instagramImg/IGimg3.jpg',
         'location': 'Hamburg, Germany',
-        'description': 'War schön, vor allem die Shishabar!'
+        'description': 'War schön, vor allem die Shishabar!',
+        'comments': [],
+        'users': []
     }
 ];
 
@@ -30,25 +36,53 @@ function render() {
         <div class="post">
             <div class="aboveImg">
                 <div class="aboveImgLeft">
-                    <img src="./instagramImg/anonym.png" alt="anonym">
+                    <a href="#"><img src="./instagramImg/anonym.png" alt="anonym"></a>
                     <div class="profileAboveImg">   
-                        <div class="author" >${post['author']}</div>
-                        <div class="location" >${post['location']}</div>
+                    <a href="#"><div class="author" >${post['author']}</div></a>
+                    <a href="#"><div class="location" >${post['location']}</div></a>
                     </div>                
                 </div>
-                    <img id="dots" src="./instagramImg/dots.png" alt="dots">
+                    <a href="#"><img id="dots" src="./instagramImg/dots.png" alt="dots"></a>
             </div>
             <img class="img" src="${post['image']}">
             <div class="postText">
-                <div class="author" >${post['author']}</div>
+                <a href="#"><div class="author" >${post['author']}</div></a>
                 <div class="description" >${post['description']}</div>
+                <div class="allComments" id="allComments${i}"></div>
             </div>
             <div class="postArea">
-                <textarea name="" id="comment" cols="30" rows="10" placeholder="Kommentar hinzufügen..."></textarea>
-                <button>Posten</button>
+                <input class="user" id="user${i}" type="text" placeholder="Name..." required>
+                <textarea name="" class="comment" id="comment${i}" cols="30" rows="10" placeholder="Kommentar..." required></textarea>
+                <button onclick="addComment(${i})">Posten</button>
             </div>
 
         </div>
         `
+
+        let commentContent = document.getElementById(`allComments${i}`);
+        
+        for (let j = 0; j < post['comments'].length; j++) {
+            const comment = post['comments'][j];
+            const user = post['users'][j];
+
+            commentContent.innerHTML += `<div><b>${user}:</b>&nbsp;${comment}</div>`;
+        }
     }
+}
+
+function addComment(index) {
+    let input = document.getElementById(`comment${index}`);
+    let userName = document.getElementById(`user${index}`);
+
+    if(input.value.trim() === '' || userName.value.trim() === '') {
+        alert('Beide Felder sind erforderlich.');
+        return;
+    }
+
+    posts[index]['comments'].push(input.value);
+    posts[index]['users'].push(userName.value);
+
+
+    render();
+    input.value = '';
 }
